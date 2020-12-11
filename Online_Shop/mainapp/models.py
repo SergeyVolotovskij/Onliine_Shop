0d23747aa51a29661f5f4db08b8ec724f593b0ab
SergeyVolotovskij <61363925+SergeyVolotovskij@users.noreply.github.com>
2020-12-12 02:03:24 +0300

    Update models.py

--- Online_Shop/mainapp/models.py
+++ Online_Shop/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#Прикинем какие модели должны у нас быть в приложении:
+
+#****************
+#1 Category (Категория)
+#2 Product (Продукт)
+#3 CartProduct (Продукт в корзине)
+#4 Cart (Сама корзина)
+#5 Order (Заказ)
+#****************
+
+#6 Customer (Покупатель)
+#7 Specification (Характеристики)
+
+class Category(models.Model):
+    passf
