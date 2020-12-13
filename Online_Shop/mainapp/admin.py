15a5f6ccb011d0643a15f70f8be628f3f2ca8610
SergeyVolotovskij <61363925+SergeyVolotovskij@users.noreply.github.com>
2020-12-14 00:11:40 +0300

    create admin

--- Online_Shop/mainapp/admin.py
+++ Online_Shop/mainapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from django import forms
 # Register your models here.
+from .models import *
+
+class NotebookCategoryChoiseField(forms.ModelChoiceField):
+    pass
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiseField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+
+class SmartphoneCategoryChoiseField(forms.ModelChoiceField):
+    pass
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return SmartphoneCategoryChoiseField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- Online_Shop/mainapp/models.py
+++ Online_Shop/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model() #мы хотим использовать Юзера, который создан в settings.AUTH.USER_MODEL
 
@@ -26,6 +27,10 @@ class Category(models.Model): #Категория
         return self.name
 
 class Product(models.Model): #Продукт
+
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE) #категория, к которой товар и принадлежит
     title = models.CharField(max_length=255, verbose_name='Наименование продукта') #наименование продукта
     slug = models.SlugField(unique=True)
@@ -40,8 +45,11 @@ class Product(models.Model): #Продукт
 
 class CartProduct(models.Model): #Продукт в корзине
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE) #юзер которому пренадлежит продукт
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE) #корзина
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE) #продукт, который кладем в корзину
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products') #корзина
+    #product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE) #продукт, который кладем в корзину
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    contetnt_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1) #количество
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена') #финальная цена
 
@@ -51,7 +59,7 @@ class CartProduct(models.Model): #Продукт в корзине
 
 class Cart(models.Model): #Корзина
     owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)  #владелец
-    products = models.ManyToManyField(CartProduct, blank=True) #многие ко многим
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart') #многие ко многим
     total_products = models.PositiveIntegerField(default=0) #для показа корректного количества товара в корзине
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена') #финальная цена
 
@@ -67,13 +75,29 @@ class Customer(models.Model): #Покупатель
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
-class Specifications(models.Model): #Характеристики продукта
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ экрана')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы')
 
     def __str__(self):
-        return "Характеристики для товара: {}".format(self.name)
+        return "{} : {}".format(self.category.name, self.title)
 
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ экрана')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объем встраиваемой памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
 
 
